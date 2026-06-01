3ceab4bca1fad633255619ac0e29af3e116c8e96
21yrm <yaorunmao@gmail.com>
2026-06-01 17:37:43 +0900

    Fix citation stats workflow timeout

--- google_scholar_crawler/main.py
+++ google_scholar_crawler/main.py
@@ -1,14 +1,14 @@
 from scholarly import scholarly
-import jsonpickle
 import json
 from datetime import datetime
 import os
 
+scholar_id = os.environ['GOOGLE_SCHOLAR_ID']
+print(f"Fetching Google Scholar stats for {scholar_id}", flush=True)
+
 author: dict = scholarly.search_author_id(os.environ['GOOGLE_SCHOLAR_ID'])
-scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
-name = author['name']
+scholarly.fill(author, sections=['basics', 'indices', 'counts'])
 author['updated'] = str(datetime.now())
-author['publications'] = {v['author_pub_id']:v for v in author['publications']}
 print(json.dumps(author, indent=2))
 os.makedirs('results', exist_ok=True)
 with open(f'results/gs_data.json', 'w') as outfile:
